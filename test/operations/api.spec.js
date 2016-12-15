f9b4b73ff26f08502ef520d8a532c8923921d086
Markus Tacker <m@coderbyheart.com>
2016-12-15 14:40:47 +0100

    test(API): refactor tests to better yield errors

--- test/operations/api.spec.js
+++ test/operations/api.spec.js
@@ -8,50 +8,61 @@ const api = require('../../src/api')
 const headers = {'Content-type': api.CONTENT_TYPE}
 import {filter, head} from 'lodash/fp'
 import {Status} from '../../src/operations/status'
+import Promise from 'bluebird'
 
 describe('API', () => {
   describe('/', () => {
-    it('should return the API index', done => {
+    it('should return the API index', () => {
       const path = '/'
       const httpMethod = 'GET'
-      index
-        .handler({
-          headers,
-          httpMethod,
-          path
-        }, null, (err, res) => {
-          if (err) return done(err)
+      return new Promise(
+        (resolve, reject) => {
+          index
+            .handler({
+              headers,
+              httpMethod,
+              path
+            }, null, (err, res) => {
+              if (err) return reject(err)
+              return resolve(res)
+            })
+        })
+        .then(res => {
           expect(res.statusCode).to.equal(200)
           expect(res.headers).to.deep.equal({
             'Content-Type': api.CONTENT_TYPE
           })
           const body = JSON.parse(res.body)
-          expect(body.$links.length).to.be.at.least(4)
+          expect(body.$links.length).to.be.at.least(2)
           const statusLink = head(filter({$context: Status.$context.toString()})(body.$links))
           expect(statusLink.$context).to.equal(Status.$context.toString())
-          done()
         })
     })
   })
   describe('/status', () => {
-    it('should return status', done => {
+    it('should return status', () => {
       const path = '/status'
       const httpMethod = 'POST'
-      index.handler({
-        headers,
-        httpMethod,
-        path
-      }, null, (err, res) => {
-        expect(err).to.equal(null)
-        expect(res.statusCode).to.equal(200)
-        expect(res.headers).to.deep.equal({
-          'Content-Type': api.CONTENT_TYPE
+      return new Promise(
+        (resolve, reject) => {
+          index.handler({
+            headers,
+            httpMethod,
+            path
+          }, null, (err, res) => {
+            if (err) return reject(err)
+            return resolve(res)
+          })
         })
-        const body = JSON.parse(res.body)
-        expect(body.status).to.equal('ok')
-        expect(new Date(body.time).getTime()).to.be.most(Date.now())
-        done()
-      })
+        .then(res => {
+          expect(res.statusCode).to.equal(200)
+          expect(res.headers).to.deep.equal({
+            'Content-Type': api.CONTENT_TYPE
+          })
+          const body = JSON.parse(res.body)
+          expect(body.status).to.equal('ok')
+          expect(new Date(body.time).getTime()).to.be.most(Date.now())
+        })
     })
   })
 })
