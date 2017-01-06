1a736a5766dbb76108308ed7574f5b21253d523d
Markus Tacker <m@coderbyheart.com>
2017-01-06 14:37:02 +0100

    fix(dependencies): updated status

--- src/operations/status.js
+++ src/operations/status.js
@@ -3,9 +3,5 @@ import {Status} from 'rheactor-models'
 import config from '../config'
 
 export const handler = {
-  post: () => Promise.resolve(new Status({
-    status: 'ok',
-    time: new Date(),
-    version: config.get('version') + '+' + config.get('environment') + '.' + config.get('deploy_time')
-  }))
+  post: () => Promise.resolve(new Status('ok', new Date(), config.get('version') + '+' + config.get('environment') + '.' + config.get('deploy_time')))
 }
